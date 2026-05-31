b918d995e5ca4c934b01ccb853ec230a4454739c
Aayush-Waje <awaje@purdue.edu>
2026-05-31 10:05:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function authenticate(userId, password) {
-    if (userId == "admin" && password == "secret") {
-        console.log("Welcome Admin!");
-        document.querySelector(".output").innerHTML =
-            "Welcome Admin!";
+const checkLogin = (userId, password) => {
+    let message = "";
+    if (userId === "admin" && password === "secret") {
+        message = "Welcome Admin!";
     }
     else {
-        console.log("BAD LOGIN CREDENTIALS");
-        document.querySelector(".output").innerHTML =
-            "BAD LOGIN CREDENTIALS";
+        message = "BAD LOGIN CREDENTIALS";
     }
-}
-function startLogin() {
-    var userId = prompt("Enter Username (Case Sensitive)");
-    var password = prompt("Enter Password (Case Sensitive)");
-    authenticate(userId, password);
-}
+    console.log(message);
+    document.querySelector(".output").innerHTML = message;
+};
+const startLogin = () => {
+    const userId = prompt("Enter Username (Case Sensitive):");
+    const password = prompt("Enter Password (Case Sensitive):");
+    checkLogin(userId, password);
+};
